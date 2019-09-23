208160f271ea6a1f10ac6dffcbb94c2dfb1353ef
bare612 <salaammn@gmail.com>
2019-09-23 09:37:57 -0500

    Generic-Cnn-Module

---
+++ Cnn/src/test/java/home/TestNews.java
@@ -0,0 +1,18 @@
+package home;
+
+import common.CommonAPI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNews extends CommonAPI {
+@Test
+    public void test1() {
+
+    String title = driver.getTitle();
+    String actualTitle="CNN - Breaking News, Latest News and Videos";
+    Assert.assertEquals(actualTitle,title);
+
+    }
+
+}
+
